f6c126b631044ff5898411a2213770a6cd0537df
Matheus <matheus.ads.study@gmail.com>
2019-05-10 21:59:01 -0300

    5 - Realizando o envio de e-mail

--- src/test/java/enviando/mail/AppTest.java
+++ src/test/java/enviando/mail/AppTest.java
@@ -2,9 +2,14 @@ package enviando.mail;
 
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Authenticator;
+import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.junit.Test;
 
@@ -13,7 +18,7 @@ import junit.framework.TestCase;
 public class AppTest extends TestCase {
 	
 	private String userName = "matheuseason2019@gmail.com";
-	private String senha = "seanson2019";
+	private String senha = "season2019";
 
 	@Test
 	public void testeEmail() {
@@ -35,6 +40,26 @@ public class AppTest extends TestCase {
 				}
 			});
 			
+//			Estabelecendo pra quem vai ser enviado - no caso 3 destinatários
+			Address[] toUser = InternetAddress.parse("matheus.ads.study@gmail.com, matheus.sungoddess@gmail.com, matheuseason2019@gmail.com");
+			
+//			Será nossa mensagem:
+			Message message = new MimeMessage(session);
+			
+//			Quem esta enviando?
+			message.setFrom(new InternetAddress(userName));
+			
+//			Pra quem está enviando - que será a lista de emails
+			message.setRecipients(Message.RecipientType.TO, toUser);
+			
+//			Assunto do Email:
+			message.setSubject("Chegou o e-mail enviado com java");
+			
+//			Corpo do Email
+			message.setText("Olá programador, voce acada de receber um email");
+			
+//			Transportar
+			Transport.send(message);
 
 		} catch (Exception e) {
 			e.printStackTrace();
