5db2ae41b5877d62dc2c5d2fa4493df0b68c8612
Matheus <matheus.ads.study@gmail.com>
2019-05-10 21:19:22 -0300

    ** 4 - Configurando conexão com servidor Gmail

--- src/test/java/enviando/mail/AppTest.java
+++ src/test/java/enviando/mail/AppTest.java
@@ -1,5 +1,10 @@
 package enviando.mail;
+
 import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 
 import org.junit.Test;
 
@@ -7,21 +12,33 @@ import junit.framework.TestCase;
 
 public class AppTest extends TestCase {
 	
+	private String userName = "matheuseason2019@gmail.com";
+	private String senha = "seanson2019";
+
 	@Test
 	public void testeEmail() {
-		
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");//Autorização.
-		properties.put("mail.smtp.starttls", "true"); //Referente a parte de segurança - Autenticação.
-		properties.put("mail.smtp.host", "smtp.gmail.com");//Servidor do Gmail Google.
-		properties.put("mail.smtp.port", "465"); //Porta do servidor - caminho de entrada pro servidor
 
-//		Definindo um ponto final de fluxo comunicação
-		properties.put("mail.smtp.socketFactory.port", "465"); //Especifica a porta a ser conectada pela socket
-		
-//		Classe do Java que vai usar pra fazer esse tipo de connection
-		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//Classe socket de conexao ao SMTP
-		
+		try {
+
+			Properties properties = new Properties();
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.starttls", "true");
+			properties.put("mail.smtp.host", "smtp.gmail.com");
+			properties.put("mail.smtp.port", "465");
+			properties.put("mail.smtp.socketFactory.port", "465");
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			
+			Session session = Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(userName, senha);
+				}
+			});
+			
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-   
+
 }
